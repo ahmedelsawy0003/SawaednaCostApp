37c09e7511cdcd62d5e3393e6bf61bc2cdc9a293
Ahmed El-Sawy <ahmad.m.alsawy@gmail.com>
2025-09-07 20:17:56 +0300

    015

--- app/models/contractor.py
+++ app/models/contractor.py
@@ -6,7 +6,7 @@ class Contractor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False, unique=True)
     contact_person = db.Column(db.String(255))
-    phone_number = db.Column(db.String(50))
+    phone = db.Column(db.String(50))
     email = db.Column(db.String(255))
 
     items = db.relationship('Item', back_populates='contractor', lazy=True)
