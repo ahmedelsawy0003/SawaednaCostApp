604d8076333bcf53fb3df2e645c2587d94f92c4d
Ahmed El-Sawy <ahmad.m.alsawy@gmail.com>
2025-09-08 11:14:16 +0300

    025

--- app/__init__.py
+++ app/__init__.py
@@ -20,14 +20,11 @@ def create_app():
     app.config.from_object('config.Config')
 
     db.init_app(app)
-    # We remove migrate from init, as we will handle schema creation manually
-    # migrate.init_app(app, db) 
+    migrate.init_app(app, db) 
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
     
     commands.init_app(app)
-
-    migrate.init_app(app, db)
 
     @login_manager.user_loader
     def load_user(user_id):
@@ -35,13 +32,8 @@ def create_app():
 
     @app.context_processor
     def inject_sidebar_data():
-        """
-        Injects data needed for the sidebar into all templates.
-        Fetches projects and contractors based on user role.
-        """
         sidebar_projects = []
         sidebar_contractors = []
-
         if current_user.is_authenticated:
             if current_user.role in ['admin', 'sub-admin']:
                 sidebar_projects = Project.query.filter_by(is_archived=False).order_by(Project.name).all()
@@ -51,7 +43,6 @@ def create_app():
                 if sidebar_projects:
                     project_ids = [p.id for p in sidebar_projects]
                     sidebar_contractors = Contractor.query.join(Invoice).filter(Invoice.project_id.in_(project_ids)).distinct().order_by(Contractor.name).all()
-
         return dict(
             sidebar_projects=sidebar_projects,
             sidebar_contractors=sidebar_contractors
@@ -65,6 +56,7 @@ def create_app():
     from .routes.contractor_routes import contractor_bp
     from .routes.invoice_routes import invoice_bp
     from .routes.cost_detail_routes import cost_detail_bp
+    from .routes.payment_routes import payment_bp  # <-- سطر الاستيراد المضاف
 
     app.register_blueprint(project_bp)
     app.register_blueprint(item_bp)
@@ -73,6 +65,7 @@ def create_app():
     app.register_blueprint(contractor_bp)
     app.register_blueprint(invoice_bp)
     app.register_blueprint(cost_detail_bp)
+    app.register_blueprint(payment_bp)  # <-- سطر التسجيل المضاف
     
     @app.route('/')
     def index():
